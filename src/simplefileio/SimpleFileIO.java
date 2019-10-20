e8051ef483db2fd4c0a26647c8a24101aa9b5701
Thor <thor@10.14.6>
2019-10-20 14:01:38 +0200

    next iter

--- src/simplefileio/SimpleFileIO.java
+++ src/simplefileio/SimpleFileIO.java
@@ -1,5 +1,6 @@
 package simplefileio;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class SimpleFileIO {
@@ -8,7 +9,7 @@ public class SimpleFileIO {
 	/**
 	 * @param args the command line arguments
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO code application logic here
 		filename = "Data/test.csv";
 		MyFileIO.readFromFile(filename);
@@ -16,9 +17,11 @@ public class SimpleFileIO {
 	
 	static class MyFileIO {
 		
-		public static void readFromFile(String filename) {
-			Scanner myScanner = new Scanner(filename);
+		public static void readFromFile(String filename) throws FileNotFoundException {
+			File file = new File(filename);
+			Scanner myScanner = new Scanner(file);
 			String line = "";
+			System.out.println("iinto write");
 			while (myScanner.hasNextLine()){
 				line = myScanner.nextLine();
 				System.out.println(line);
