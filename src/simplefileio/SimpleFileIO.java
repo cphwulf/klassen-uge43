46c77dee658d19d083f977baa79fc5f5fe2f032a
Thor <thor@10.14.6>
2019-10-20 14:47:01 +0200

    getNumOfDoors-solution

--- src/simplefileio/SimpleFileIO.java
+++ src/simplefileio/SimpleFileIO.java
@@ -12,12 +12,17 @@ public class SimpleFileIO {
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO code application logic here
 		filename = "Data/test.csv";
-		MyFileIO.readFromFile(filename);
+		//MyFileIO.readFromFile(filename);
+		String testString = "S80 34 2.5T 4dr";
+		int retValDoors = StringParsing.getNumOfDoors(testString);
+		//int retValDoors = 5;
+		System.out.println(retValDoors);
 	}
 	
 	static class StringParsing {
 		
-		public static void getNumOfDoors(String myString) {
+		public static int getNumOfDoors(String myString) {
+			int retVal = 0;
 			/*
 			find tallet umiddelbart før dr - hvis dr forekommer
 			S80 2.5T 4dr
@@ -25,8 +30,24 @@ public class SimpleFileIO {
 			IS 300 4dr manual
 			Forester X
 			*/
-
-			
+			if (myString.contains("dr")) {
+				String[] myLineArr = myString.split(" ");
+				for (int i=0; i< myLineArr.length;i++) {
+					if (myLineArr[i].contains("dr")) {
+						String tmpStr = myLineArr[i];
+						System.out.println("into action" + tmpStr);
+						char[] myChars = tmpStr.toCharArray();
+						for (int j=0;j<myChars.length;j++ ) {
+							if (myChars[j] < 57) {
+								retVal=myChars[j]-48;
+							}
+						}
+					}
+					
+				}
+				
+			}
+			return retVal;
 			
 		}
 	}
