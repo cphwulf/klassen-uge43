fbbb6a12bbf41a2e272004d7b191b80f5895d56c
Thor <thor@10.14.6>
2019-10-22 09:16:14 +0200

    adding try/catch

--- src/simpledogsio/ImportDogsFromFile.java
+++ src/simpledogsio/ImportDogsFromFile.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package simpledogsio;
 
 import java.io.File;
@@ -16,18 +16,24 @@ import java.util.Scanner;
 public class ImportDogsFromFile {
 	//Red,Dobermann,AMY,Desexed Female,WATERLOO CORNER
 	// Dog(String name, String gender, String color, String race, String cityOrigin)
-	public static void importDogsFromCSV(String filename) throws FileNotFoundException {
+	public static void importDogsFromCSV(String filename) {
+		filename = "test";
 		DogCompound myCompound = new DogCompound("C10", "Kurt");
 		String line = "";
 		File fh = new File(filename);
-		Scanner myScanner = new Scanner(fh);
-		while (myScanner.hasNextLine()) {
-			line = myScanner.nextLine();
-			String[] myArr = line.split(",");
-			Dog tmpDog = new Dog(myArr[3], myArr[4], myArr[1], myArr[2], myArr[5]);
-			myCompound.addDog(tmpDog);
+		try {
+			Scanner myScanner = new Scanner(fh);
+			while (myScanner.hasNextLine()) {
+				line = myScanner.nextLine();
+				String[] myArr = line.split(",");
+				Dog tmpDog = new Dog(myArr[3], myArr[4], myArr[1], myArr[2], myArr[5]);
+				myCompound.addDog(tmpDog);
+			}
+			myCompound.showDogs();
+			
+		} catch (Exception e) {
+			System.out.println("error: " + e);
 		}
-		myCompound.showDogs();
 	}
 	
 }
